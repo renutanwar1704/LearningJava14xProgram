95bd7abc38bf465c7fd61f053b9aad8ecf79caf4
renu tanwar <renu.tanwar1704@gmail.com>
2025-11-02 18:39:53 +0530

    TestGIT Program

---
+++ src/ex_01_JavaBasics/Lab000_TestGITPushCode.java
@@ -0,0 +1,18 @@
+package ex_01_JavaBasics;
+import java.util.Scanner;
+public class Lab001_HelloWorld {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter Username");
+        System.out.println("Enter age");
+        System.out.println("Enter marks");
+        String userName = obj.nextLine();
+        System.out.println("Username is " + userName);
+
+        int age = obj.nextInt();
+        System.out.println("Age is" + age);
+
+        float marks = obj.nextFloat();
+        System.out.println("Marks are " + marks);
+    }
+}
