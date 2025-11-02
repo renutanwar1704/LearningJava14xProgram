f761e4a9e7a0772992cb74343d3c6be1017d7845
renu tanwar <renu.tanwar1704@gmail.com>
2025-11-02 18:55:14 +0530

    Helloworld Program

---
+++ src/ex_01_JavaBasics/Lab00_HelloWorld.java
@@ -0,0 +1,8 @@
+package ex_01_JavaBasics;
+// import java.util.Scanner;
+public class Lab00_HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+    }
+}
